04de499cf85b5e5e60e05f5a5c7277dbdf4b0f7d
yony <leehayaon@naver.com>
2022-06-13 15:29:10 +0900

    Vue v-html 데이터바인딩

--- vue-default/src/router/index.js
+++ vue-default/src/router/index.js
@@ -38,6 +38,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "basic" */ '../views/DataBinding.vue')
+  },
+  {
+    path: '/databindinghtml',
+    name: 'DataBindingHtml',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "basic" */ '../views/DataBindingHtml.vue')
   }
 ]
 
---
+++ vue-default/src/views/DataBindingHtml.vue
@@ -0,0 +1,22 @@
+<template>
+<div v-html="htmlString">
+</div>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line
+  name: '',
+  components: {},
+  data(){
+    return {
+      htmlString: '<p style="color:red">This is a red string.</p>'
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+  }
+}
+</script>
