e6b4f2b26f168ee0744640f81a1a1d0c69ff3f58
yony <leehayaon@naver.com>
2022-06-13 11:15:41 +0900

    Vue component 구조 만들기

--- vue-default/src/router/index.js
+++ vue-default/src/router/index.js
@@ -22,6 +22,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact", webpackPrefetch : true */ '../views/ContactView.vue')
+  },
+  {
+    path: '/basic',
+    name: 'basic',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "basic", eslint-disable */ '../views/BasicView.vue')
   }
 ]
 
