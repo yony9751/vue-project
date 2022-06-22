bab3881793f1d90396027b9cb3cbcbeb97e5a5ee
yony <leehayaon@naver.com>
2022-06-22 15:31:05 +0900

    Vue 컴포넌트 개발 / 부모 컴포넌트에서 자식 컴포넌트로 데이터 전달

---
+++ vue-default/src/components/PageTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2 class="page-title">페이지 타이틀 {{title}}입니다</h2>
+</template>
+<script>
+export default {
+  props : {
+    title : {
+      type : String,
+      default : '1번 페이지'
+    }
+  }
+}
+</script>
+<style scoped>
+.page-title{font-size:18px;padding-left:10px;border-left: 5px solid red;}
+</style>
---
+++ vue-default/src/views/ChildComponent.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  likes : {{likes}} <br>
+  isOk : {{isOk}}<br>
+  <div>{{ author.name }} {{ author.company }}</div>
+</div>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line
+  name: 'ChildComponent',
+  props : {
+      likes : {
+        type : Number,
+        default : 23
+      },
+      isOk : {
+        type : Boolean,
+        default : false
+      },
+      commentIds :{
+        type : Array,
+      },
+      author : {
+        type : Object
+      }
+    },
+  components: {},
+  data(){
+    return {
+      sampleData: ''
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+  }
+}
+</script>
