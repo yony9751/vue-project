664a89fd083369db68eac434cd2c1f7d24dc2dae
yony <leehayaon@naver.com>
2022-06-29 15:23:20 +0900

    Vue Slot Modal 만들기

---
+++ vue-default/src/views/SlotUseModalLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <modal-layout>
+    <template v-slot:header>
+      <h1>헤더입니다</h1>
+    </template>
+    <template v-slot:default>
+      <p>메인내용</p>
+    </template>
+    <template v-slot:footer>
+      <button type="button">닫기</button>
+    </template>
+  </modal-layout>
+</template>
+<script>
+import SlotModalLayoutVue from './SlotModalLayout.vue';
+export default {
+  // eslint-disable-next-line
+  name: '',
+  components: {
+    'modal-layout' : SlotModalLayoutVue
+  },
+  data(){
+    return {
+      sampleData: ''
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+  }
+}
+</script>
