d614134e201db56f95cfebb654f06771c379b410
yony <leehayaon@naver.com>
2022-07-06 10:46:01 +0900

    Vue 컴포지션 API setup(), reactive

---
+++ vue-default/src/views/Calculator.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Calculator</h2>
+    <div>
+      <input type="text" v-model.number="num1" @keyup="plusNumbers">
+      <span> + </span>
+      <input type="text" v-model.number="num2" @keyup="plusNumbers">
+      <span> = </span>
+      <span>{{result}}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line
+  name: 'calculator',
+  components: {},
+  data(){
+    return {
+      num1 : 0,
+      num2 : 0,
+      result : 0
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    plusNumbers(){
+      this.result = this.num1 + this.num2 ;
+    }
+  }
+}
+</script>
